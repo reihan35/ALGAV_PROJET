fef3353a46c2dfa8c9ce3001ed261c78c18ae771
reihan35 <reihaneh77@yahoo.com>
2018-10-18 13:09:52 +0200

    tree

--- projet.py
+++ projet.py
@@ -53,17 +53,79 @@ def union(tas1,tas2):
     l = tas1 +  tas2 
     ConsIter(l)
 
-                   
+class Noeud:
+
+    def __init__(self, cle): 
+        self.d = None
+        self.g = None
+        self.p = None
+        self.cle = cle
+
+    def print_arbre(self):
+        if self.g:
+            self.g.print_arbre()
+        
+        print(self.cle)
+        
+        if self.d:
+            self.d.print_arbre()
+            
+    def ajout_fin(self,cle):
+        if self.g is None :
+            self.g = Noeud(cle)
+
+        else:   
+            self.g.ajout_fin(cle)
+    
+    def der(self):
+        if self.g is None :
+             self
+        else:   
+            self.g.der()
+    
+        
+    def remonte_elem(self,n):
+        #if self.p is None:
+            self         
+        #else:
+            if self.p.cle<n.cle:
+                tmp=self.p.cle
+                n.cle=self.p.cle
+                self.p.cle=tmp
+                remonte_elem(self.p,n)
+            else:
+                self
+    
+    def ajout(self,cle):
+        self.ajout_fin(cle)
+        #self.remonte_elem(self.der())
+                    
+    
 def main():
     fic = "cles_alea/jeu_5_nb_cles_50000.txt"
     content = parse_file(fic)
     #print(content)
-    print(len(content))
-    print(eg(content[0], content[2]))
-    print(inf(content[4], content[0]))
+    #print(len(content))
+    #print(eg(content[0], content[2]))
+    #print(inf(content[4], content[0]))
     
     tas = [2,5,6,10,13,8, 7]
     tas2=ConsIter([1,5,3,0,8,2])
-    print(tas2)
+    #print(tas2)
+    
+    root = Noeud(12)
+    root.ajout_fin(6)
+    root.ajout_fin(7)
+    root.ajout_fin(3)
+    
+    root.print_arbre()
+    
+    root.remonte_elem(Noeud(3))
+    
+    root.print_arbre()
+    #root.ajout(6)
+    
 
+    #root.print_arbre()
+    
 main()
